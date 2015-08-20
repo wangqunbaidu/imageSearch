b149833d063184c8a442d9e7817529c0ea0e108b
wangqun <wangqun@baidu.com>
2015-08-20 10:33:16 +0800

    加入flash判断

--- src/imageSearch.js
+++ src/imageSearch.js
@@ -281,7 +281,7 @@ imageSearch.prototype = {
         addEvent(content, 'drop', function(e) {
             e.stopPropagation();
             e.preventDefault();
-            me.isSubmit=1;
+            me.isSubmit = 1;
             
             if (e.dataTransfer.files.length) {
                 try {
@@ -333,6 +333,38 @@ imageSearch.prototype = {
         nodes = null;
         fragment = null;
     },
+    /**
+     * 以下为判断是否安装flash
+     */
+
+    flashChecker: function() {
+        var hasFlash=0;　　　　//是否安装了flash
+        var flashVersion=0;　　//flash版本
+
+        if(document.all) {
+            var swf = new ActiveXObject('ShockwaveFlash.ShockwaveFlash');
+            if(swf) {
+                hasFlash = 1;
+                VSwf = swf.GetVariable("$version");
+                flashVersion = parseInt(VSwf.split(" ")[1].split(",")[0]);
+            }
+        }
+        else {
+            if (navigator.plugins && navigator.plugins.length > 0) {
+                var swf = navigator.plugins["Shockwave Flash"];
+                if (swf) {
+                    hasFlash=1;
+                    var words = swf.description.split(" ");
+                    for (var i = 0; i < words.length; ++i) {
+                        if (isNaN(parseInt(words[i]))) continue;
+                        flashVersion = parseInt(words[i]);
+                    }
+                }
+            }
+        }
+        return {f:hasFlash,v:flashVersion};
+    },
+
     /**
      * 初始化入口显示状态
      */
